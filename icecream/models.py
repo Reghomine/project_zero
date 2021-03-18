b5a167d16f2fbf7d92b828c4373c254bde8b21f5
Reghomine <65400439+Reghomine@users.noreply.github.com>
2021-03-18 12:25:10 +0300

    Добавил запрос бд
    Вот так вот

--- icecream/models.py
+++ icecream/models.py
@@ -11,44 +11,5 @@ class Icecream(models.Model):
     rating = models.IntegerField()
 
 
-icecream_db = [
-    {
-        'name':'Коровка из кореновки',
-        'desc':'Шокаладка, вафелька Вауся , персик',
-        'avatar':'https://shop.korenovka.ru/d/1_2.jpg',
-        'rad': 4,
-    },
-    {
-        'name':'Максибон',
-        'desc':'Шокаладка, бисквит ',
-        'avatar':'https://shop.korenovka.ru/thumb/2/7zkWC6WzJWACc5XFf1khvg/800r800/d/mulyaz_stakan-3d-beliy_0.jpg',
-        'rad': 5,
-    },
-    {
-        'name':'45копеек',
-        'desc':'Шокаладка, вафля ',
-        'avatar':'https://shop.korenovka.ru/thumb/2/9lXdj_ohqp3y80FiY-xIGw/800r800/d/3_2.jpg',
-        'rad': 1,
-    },
-    {
-        'name':'Фруктовое',
-        'desc':'Клубника, яблоко,  вода ',
-        'avatar':'https://shop.korenovka.ru/thumb/2/fhfQ_dUklElGwYv9lGvtMA/800r800/d/2_1.jpg',
-        'rad': 2,
-    },
-    {
-        'name':'Мороженое из мака',
-        'desc':'Куча всякого заменителя  ',
-        'avatar':'https://shop.korenovka.ru/thumb/2/esDfmWyAYB409GOZe-vg5Q/800r800/d/4_1.jpg',
-        'rad': 4,
-    },
-    {
-        'name':'Ноу нейм марка',
-        'desc':'"Не понятный мелкий шрифт"  ',
-        'avatar':'https://shop.korenovka.ru/thumb/2/8NiwjNuG4Z-OeDUEh2jIgg/800r800/d/eskimo_v_shokoladnoy_glazuri_korenovka.jpg',
-        'rad': 0,
-    },
-    
-]
 
 
--- icecream/views.py
+++ icecream/views.py
@@ -1,27 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
+from .models import Icecream
 
 def icecream_list(request):
-    icecream = []
-    for i in range(len(icecream_db)):
-        temp = icecream_db[i]
-        temp['index'] = i
-        icecream.append(temp)
-        #icecream += f"<a href = '{i}'>{icecream_db[i]['name']}</a><br>"
+    icecream = Icecream.objects.all()
+    
     context = {'icecream':icecream}
     return render(request, 'icecream/icecream-list.html', context)
 
 def icecream_detail(request, pk):
-    name = icecream_db[pk]['name']
-    avatar = icecream_db[pk]['avatar']
-    desc = icecream_db[pk]['desc']
-    rad = icecream_db[pk]['rad']
+    icecream = get_object_or_404(Icecream, pk=pk)
+    rating = int(icecream.rating)
     context = {
-        'name':name,
-        'avatar':avatar,
-        'desc':desc,
-        'range':range(rad),
-        'zero_range':range(5-rad),
+        'icecream':icecream,
+        'range':range(rating),
+        'zero_range':range(5-rating),
     }
     return render(request, 'icecream/icecream-detail.html', context)
